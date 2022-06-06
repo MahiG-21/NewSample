c397b6091c72797ae2d67f655a585a4034bd4ae0
MahiG-21 <mahima21.goray@gmail.com>
2022-06-05 20:20:19 -0400

    he

--- src/test/java/GitDemo/WikiPageTestNG.java
+++ src/test/java/GitDemo/WikiPageTestNG.java
@@ -18,7 +18,13 @@ public void setup()
     driver.get("https://www.wikipedia.org/");
     driver.manage().window().maximize();
     System.out.println("Before Methos will Execute ");
-   
+    System.out.println("Before Methos will Execute ");
+
+    System.out.println("Before Methos will Execute ");
+    System.out.println("Before Methos will Execute ");
+    System.out.println("Before Methos will Execute ");
+    System.out.println("Before Methos will Execute ");
+
 
     
     
